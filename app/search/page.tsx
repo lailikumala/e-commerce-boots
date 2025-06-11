b7cf978c4db68dd51805248f0cb936d916a0e7ca
Laili Kumala <laili.kumala-consultant@prudential.co.id>
2025-06-11 22:53:26 +0700

    create new screen search and detail product

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,9 +1,55 @@
-import React from 'react'
+"use client"
+
+import axios from 'axios';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useSearchParams } from 'next/navigation'
+import React, { Suspense, useEffect, useState } from 'react'
+
+interface IProduct {
+  _id: string;
+  image: string;
+  name: string;
+  price: number;
+}
+
+const SearchComponent = () => {
+  const [products, setProducts] = useState([]);
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const searchTermFromUrl = searchParams.get("searchTerm");
+    if(searchTermFromUrl) {
+      axios
+      .get(`/api/search?searchTerm=${searchTermFromUrl}`)
+      .then((response) => setProducts(response.data.products))
+      .catch((error) => console.log("Error fetching serach result:", error))
+    }
+  },[searchParams]);
+
+
+
+  return (
+    <div id='product' className='px-4 md:px-12 py-5 md:py-10 flex justify-center items-center'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5'>
+        {products.map((product: IProduct, index) => (
+          <Link href={`/product/${product._id}`} key={index}>
+            <Image src={product.image} alt="dummy-img" width={1000} height={1000} className='max-w-[17rem] h-72 object-cover object-center rounded-lg'/>
+            <div className='mt-4'>
+              <h2 className='font-semibold text-lg'>{product.name}</h2>
+              <p className='font-medium text-sm mt-1'>{`$${product.price}`}</p>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
+}
 
 const SearchPage = () => {
-  return (
-    <div>SearchPage</div>
-  )
+  return <Suspense fallback={"Hello"}>
+    <SearchComponent/>
+  </Suspense>
 }
 
 export default SearchPage
