7f91aa879077174fa0c7efe861e7fcffe199e9c7
Laili Kumala <laili.kumala-consultant@prudential.co.id>
2025-06-13 22:06:14 +0700

    change theme

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,8 +7,8 @@ const Hero = () => {
   return (
     <div className='min-h-[70vh] md:min-h-[60vh] lg:min-h-[90vh] flex flex-col md:flex-row justify-center items-center bg-white px-4 md:px-12 text-black'>
       <div className='max-w-2xl'>
-        <h1 className='text-5xl pt-6 md:pt-0 md:text-7xl leading-tight font-semibold'>Timeless Elegance on Your Wrist</h1>
-        <p className='text-[#495957] mt-4'>Discover our curated collection of premium watches, crafted for those who appreciate sophistication and precision</p>
+        <h1 className='text-5xl pt-6 md:pt-0 md:text-7xl leading-tight font-semibold'>Unmatched Durability for Every Adventure</h1>
+        <p className='text-[#495957] mt-4'>Step into the wild with confidence. Our premium boots are designed to withstand the toughest terrains while keeping your style rugged and refined.</p>
 
         <Link href='#product'>
           <button className='mt-8 bg-[#212529] text-white px-3 py-2 rounded-md cursor-pointer'>Shop the Collection</button>
