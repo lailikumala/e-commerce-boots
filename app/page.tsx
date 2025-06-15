a00d809a60ed8a1b40f1425d849975bd1c3e428d
Laili Kumala <laili.kumala-consultant@prudential.co.id>
2025-06-15 12:21:34 +0700

    service-worker move to root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
+"use client"
+
 import Hero from "@/components/Hero";
 import ProductList from "@/components/ProductList";
+import { useEffect } from "react";
 
 export default function Home() {
+
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.getRegistrations().then(regs => regs);
+    }
+
+    if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => reg)
+      .catch(err => err);
+    }
+  },[])
+
   return (
     <div className="bg-[#F8F9FA] ">
       <Hero/>
